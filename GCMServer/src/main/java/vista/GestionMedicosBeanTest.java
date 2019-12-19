aab73c243cc412d69eacb35188dd52fc1f4c20c4
Alejandro Enriquez <AlejoEnriquez2>
2019-12-19 10:39:56 -0500

    Subir

---
+++ GCMServer/src/main/java/vista/GestionMedicosBeanTest.java
@@ -0,0 +1,104 @@
+package vista;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestionMedicosBeanTest {
+
+	@Test
+	public void testGetGl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUs_codigo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUs_codigo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUs_nombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUs_nombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUs_fechaNacimiento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUs_fechaNacimiento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUs_nickname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUs_nickname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUs_password() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUs_password() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMedicos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMedicos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGuardarUsuarios() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarMedicos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+}
