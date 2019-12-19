9c18fb7399f38831405b62ae1bba1249c2590590
Alejandro Enriquez <AlejoEnriquez2>
2019-12-19 11:54:02 -0500

    Daos

--- GCMServer/src/main/java/datos/RecetaMedicaDAO.java
+++ GCMServer/src/main/java/datos/RecetaMedicaDAO.java
@@ -1,5 +1,48 @@
 package datos;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import modelo.Cita;
+import modelo.RecetaMedica;
+
+@Stateless
 public class RecetaMedicaDAO {
 
+
+	@Inject
+	private EntityManager em;
+	
+	public void insert(RecetaMedica receta) {
+		em.persist(receta);
+	}
+	
+	public void update(RecetaMedica receta) {
+		em.merge(receta);
+	}
+	
+	public void remove(int codigo) {
+		RecetaMedica receta = this.read(codigo);
+		em.remove(receta);
+	}
+	
+	public RecetaMedica read(int codigo) {
+		RecetaMedica u = em.find(RecetaMedica.class, codigo);
+		return u;
+	}
+	
+	public List<RecetaMedica> getCitas(){
+		String jpql = "SELECT u FROM Paciente u";
+		Query q = em.createQuery(jpql, RecetaMedica.class);
+		
+		List<RecetaMedica> receta = q.getResultList();
+		
+		return receta;
+	}
+	
+	
 }
