1bd23977b24d3bd53fdab16c7a7451821be48931
Alejandro Enriquez <AlejoEnriquez2>
2020-01-01 17:08:40 -0500

    Creación de CRUDS de Historia clinica, receta medica

---
+++ GCMServer/src/main/java/datos/ExamenLaboratorioDAO.java
@@ -0,0 +1,47 @@
+package datos;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import modelo.Cita;
+import modelo.ExamenLaboratorio;
+
+@Stateless
+public class ExamenLaboratorioDAO {
+
+
+	@Inject
+	private EntityManager em;
+	
+	public void insert(ExamenLaboratorio examen) {
+		em.persist(examen);
+	}
+	
+	public void update(ExamenLaboratorio examen) {
+		em.merge(examen);
+	}
+	
+	public void remove(int codigo) {
+		ExamenLaboratorio examen = this.read(codigo);
+		em.remove(examen);
+	}
+	
+	public ExamenLaboratorio read(int codigo) {
+		ExamenLaboratorio u = em.find(ExamenLaboratorio.class, codigo);
+		return u;
+	}
+	
+	public List<ExamenLaboratorio> getCitas(){
+		String jpql = "SELECT u FROM Paciente u";
+		Query q = em.createQuery(jpql, ExamenLaboratorio.class);
+		
+		List<ExamenLaboratorio> examen = q.getResultList();
+		
+		return examen;
+	}
+	
+}
